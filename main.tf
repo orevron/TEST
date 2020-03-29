aad5a615c9c7fad166add9f8f46e7c6217e36a3d
Or Evron <orevron@gmail.com>
2020-03-29 11:55:45 +0300

    added maintf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_kms_key" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+resource "aws_kms_key" "b" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+resource "aws_kms_key" "c" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
